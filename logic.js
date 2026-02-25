55731209b23bd383498440d42009e00ec6ea2f6d
Rahma Berhan <122676773+RahmaB1@users.noreply.github.com>
2026-02-25 00:21:02 +0000

    working on select and table

--- logic.js
+++ logic.js
@@ -1,3 +1,45 @@
+const userSallyData = {
+  id: "6067119dfbf00e000f893e74",
+  username: "SallyMcGrath",
+  name: "Sally McGrath",
+  honor: 1025,
+  clan: "CodeYourFuture",
+  leaderboardPosition: 35400,
+  skills: [],
+  ranks: {
+    overall: {
+      rank: -4,
+      name: "4 kyu",
+      color: "blue",
+      score: 1228,
+    },
+    languages: {
+      javascript: {
+        rank: -4,
+        name: "4 kyu",
+        color: "blue",
+        score: 1224,
+      },
+      sql: {
+        rank: -8,
+        name: "8 kyu",
+        color: "white",
+        score: 4,
+      },
+      typescript: {
+        rank: -8,
+        name: "8 kyu",
+        color: "white",
+        score: 2,
+      },
+    },
+  },
+  codeChallenges: {
+    totalAuthored: 0,
+    totalCompleted: 187,
+  },
+};
+
 export async function fetchUserData(username) {
   try {
     const response = await fetch(
@@ -16,16 +58,8 @@ export async function fetchUserData(username) {
   }
 }
 
-export async function loadUserData(userName) {
-  const userData = await fetchUserData(userName);
-  return userData;
-}
-
-// const userDataName = displayUserData(userData);
-// console.log(displayUserData(userData));
-
-const userData = await loadUserData("SallyMcGrath");
-console.log(userData.ranks.languages);
+// const userData = await fetchUserData("SallyMcGrath");
+// console.log(userData.name);
 
 // get list of the languages
 //then get the ranks for each language
@@ -39,19 +73,24 @@ console.log(userData.ranks.languages);
 //   languages,
 // );
 
-function getLanguages(userData) {
-  const languages = [];
-  if (userData) {
-    for (let data in userData) {
-      languages.push(data);
-      console.log(data);
-      //was last working here
-    }
-  }
-  //   const languageNames = Object.keys(languages);
-  //   console.log(languageNames);
-  return languages;
+export function getLanguagesNames(userData) {
+  //the return is an array of the languages keys / names
+  //[ 'javascript', 'sql', 'typescript' ]
+  return Object.keys(userData.ranks.languages);
 }
+// console.log(getLanguagesNames(userSallyData));
+
+//   if (userData) {
+//     for (let lang in userData) {
+//       languages.push(data);
+//       console.log(data);
+//       //was last working here
+//     }
+//   }
+//   //   const languageNames = Object.keys(languages);
+//   //   console.log(languageNames);
+//   return languages;
+// }
 
 // getLanguages(test);
 // if i want to work on object length==>> Object.keys(object).length
--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-import { fetchUserData } from "./logic.js";
+import { fetchUserData, getLanguagesNames } from "./logic.js";
 
 //1-  User types Codewars usernames
 //done for only one user, need to add multiple users
+//Your website should display an input,
+// allowing the user to add a comma-separated list of Codewars usernames
+//  (e.g. "CodeYourFuture,40thieves,SallyMcGrath")
+// that they want to display on the leaderboard.
 
+//------------------------------------
 const usernameInput = document.getElementById("usernameInput");
 const submitButton = document.getElementById("submitButton");
 // let username = "";
@@ -17,8 +22,26 @@ submitButton.addEventListener("click", function () {
   }
 });
 
+//------------------------------------
 // 2- You fetch their data from the Codewars API
 // done in logic.js loadUserData function , again for only one user
+const userData = await fetchUserData("SallyMcGrath");
+
+//function for only languages so that it can be used in select
+
+const languagesNames = getLanguagesNames(userData);
+console.log(languagesNames);
+const select = document.getElementById("languageSelect");
+//creating options
+languagesNames.map((lang) => {
+  const option = document.createElement("option");
+  option.value = lang;
+  option.textContent = lang;
+  select.appendChild(option);
+});
+
+//function for returning the username , clan and score for overall for now
+//then each language later
 
 //3-  You show a leaderboard table
 //whats required to show??
