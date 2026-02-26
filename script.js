8ef70af3b5157172e2e5ea063197b1edfff9a1b7
Rahma Berhan <122676773+RahmaB1@users.noreply.github.com>
2026-02-26 20:01:39 +0000

    fixing sort when selecting other langs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { fetchUserData2 } from "./temp.js";
 // glob variables
 let usersData = [];
 let nonValidUsers = [];
-let languagesNames = [];
+let languagesNamesFromAll = [];
 let selectedLanguage = "overall";
 
 //------------- Input User Name -----------------------
@@ -37,14 +37,7 @@ async function handleFetchUsersData(userNamesArray) {
     return fetchUserData2(user);
   });
   const results = await Promise.all(promises);
-  // console.log(results);
-  // console.log(results[0].data);
-  // usersData = await Promise.all(promises);
-
-  //pushin only valid data to usersData array
-  //the non vaild ones goes to non valid array
   nonValidUsers = [];
-
   for (let i = 0; i < results.length; i++) {
     if (results[i].response.status === 200) {
       usersData.push(results[i].data);
@@ -57,46 +50,7 @@ async function handleFetchUsersData(userNamesArray) {
     let names = nonValidUsers.join(", ");
     showError(messege, names);
   }
-  // console.log("test the non valid user repeation:  ", nonValidUsers);
-
-  // now we should have two arrays
-  // console.log("usersData: ", usersData);
-  // console.log("nonValidUsers: ", nonValidUsers);
-  //---------------------------------------------
-  // const userObject = {
-  //   data: usersData.data,
-  //   errorMessege: usersData.errorMessege,
-  // };
-
-  // console.log(userObject.errorMessege);
-
-  //----------------------------------------------------------
-
-  // const validUsers = [];
-  // const invalidUsers = [];
-
-  // results.forEach((result) => {
-  //   if (result.status === "fulfilled") {
-  //     validUsers.push(result.value);
-  //   } else {
-  //     invalidUsers.push(result.reason.message);
-  //   }
-  // });
-
-  // if (invalidUsers.length > 0) {
-  //   showError(`These users were not found: ${invalidUsers.join(", ")}`);
-  // }
-
-  // if (validUsers.length === 0) {
-  //   return; // nothing to render
-  // }
-
-  // usersData = validUsers;
-
-  //---------------------------------------------------------------
-  // console.log("usersData: ", usersData);
-
-  languagesNames = getLanguagesNames(usersData);
+  languagesNamesFromAll = getLanguagesNames(usersData);
   makeLangsSelect(usersData);
   renderRanks(usersData, "overall");
 }
@@ -112,62 +66,51 @@ function makeLangsSelect() {
   overallOption.textContent = "overall";
   select.appendChild(overallOption);
 
-  languagesNames.forEach((lang) => {
+  languagesNamesFromAll.forEach((lang) => {
     const option = document.createElement("option");
     option.value = lang;
     option.textContent = lang;
     select.appendChild(option);
   });
-  // console.log("line 58");
-  // console.log(languagesNames);
 }
 
 select.addEventListener("change", () => {
   selectedLanguage = select.value;
-  // console.log("Selected language:", selectedLanguage);
-  renderRanks(usersData, selectedLanguage);
+  console.log(selectedLanguage);
+
+  renderRanks();
 });
 
 function renderRanks() {
   const tableBody = document.querySelector("tbody");
   tableBody.innerHTML = "";
 
-  usersData = sortUsersDataByScore();
-  //render for multiple users and any lang
-  const languageNamesforuser = getLanguagesNames(usersData);
-  // console.log("languageNamesforuser: ", languageNamesforuser);
-
-  // console.log("line");
-  //will need to sort the usersData
-
-  for (let user = 0; user < usersData.length; user++) {
-    if (selectedLanguage === "overall") {
-      // console.log("Overall is selected");
-      // Clear existing rows
-      // tableBody.innerHTML = "";
+  if (selectedLanguage === "overall") {
+    //sort then loop and render
+    usersData = sortUsersDataByScore(usersData);
+    for (let user = 0; user < usersData.length; user++) {
       const row = document.createElement("tr");
       tableBody.appendChild(row);
-
       const usernameCell = document.createElement("td");
       const clanCell = document.createElement("td");
       const scoreCell = document.createElement("td");
-
       usernameCell.textContent = usersData[user].username;
       clanCell.textContent = usersData[user].clan;
       scoreCell.textContent = usersData[user].ranks.overall.score;
-
       row.appendChild(usernameCell);
       row.appendChild(clanCell);
       row.appendChild(scoreCell);
-      if (user === 0) {
+      if (usersData.length > 1 && user === 0) {
         row.className = "top-user";
       }
-    } else if (usersData[user].ranks.languages[selectedLanguage]) {
-      // console.log(
-      //   "Language is valid: check the score ... ",
-      //   usersData[user].ranks.languages[selectedLanguage].score,
-      // );
-
+    }
+  } else {
+    const filteredDataByLang = usersData.filter(
+      (data) => data.ranks.languages[selectedLanguage],
+    );
+    const newsorted = sortUsersDataByScore(filteredDataByLang);
+    //now we need to sort this
+    for (let user = 0; user < newsorted.length; user++) {
       const row = document.createElement("tr");
       tableBody.appendChild(row);
 
@@ -175,25 +118,22 @@ function renderRanks() {
       const clanCell = document.createElement("td");
       const scoreCell = document.createElement("td");
 
-      usernameCell.textContent = usersData[user].username;
-      clanCell.textContent = usersData[user].clan;
+      usernameCell.textContent = newsorted[user].username;
+      clanCell.textContent = newsorted[user].clan;
       scoreCell.textContent =
-        usersData[user].ranks.languages[selectedLanguage].score;
+        newsorted[user].ranks.languages[selectedLanguage].score;
 
       row.appendChild(usernameCell);
       row.appendChild(clanCell);
       row.appendChild(scoreCell);
-      if (user === 0) {
+      if (newsorted.length > 1 && user === 0) {
         row.className = "top-user";
       }
-    } else {
-      // console.log("Language is not valid:", selectedLanguage);
     }
   }
-
-  //this renders for one user - any language
 }
 
+//--------------------------------------------------------------------
 function renderRankstest() {
   // will try the overall first
 
@@ -215,7 +155,6 @@ function renderRankstest() {
   row.appendChild(scoreCell);
 }
 
-onload = renderRanks(usersData, "overall");
 //function for returning the username , clan and score for overall for now == done
 //then each language later == done
 
@@ -224,10 +163,10 @@ onload = renderRanks(usersData, "overall");
 
 // 4- User can switch between overall rank and language ranks == done
 
-export function sortUsersDataByScore() {
+export function sortUsersDataByScore(data) {
   let scoreA = 0;
   let scoreB = 0;
-  let sortedData = usersData.sort((a, b) => {
+  let sortedData = data.sort((a, b) => {
     if (selectedLanguage === "overall") {
       scoreA = a.ranks.overall.score;
       scoreB = b.ranks.overall.score;
@@ -256,5 +195,5 @@ export function sortUsersDataByScore() {
 export function showError(messege, nonValid) {
   let usernames = nonValid ? nonValid : "";
   const errorDiv = document.getElementById("error");
-  errorDiv.textContent = ` ${messege} ${usernames}`;
+  errorDiv.textContent = `${messege} ${usernames}`;
 }
